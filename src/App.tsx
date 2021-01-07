0456d27cdaea75b74797030a490c902fee95b635
Volcomix <Volcomix@users.noreply.github.com>
2021-01-07 21:00:55 +0100

    Implement basic background blur effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import * as bodyPix from '@tensorflow-models/body-pix'
 import React, { useEffect, useRef } from 'react'
 import './App.css'
 
 function App() {
-  const videoRef = useRef<HTMLVideoElement>(null)
+  const videoRef = useRef<HTMLVideoElement>()
+  const netRef = useRef<bodyPix.BodyPix>()
+  const canvasRef = useRef<HTMLCanvasElement>(null)
 
   useEffect(() => {
     async function setupCamera() {
@@ -10,16 +13,49 @@ function App() {
         const stream = await navigator.mediaDevices.getUserMedia({
           video: true,
         })
-        videoRef.current!.srcObject = stream
+        const [videoTrack] = stream.getVideoTracks()
+        const { width, height } = videoTrack.getSettings()
+
+        const video = document.createElement('video')
+        video.autoplay = true
+        video.playsInline = true
+        video.controls = false
+        video.width = width!
+        video.height = height!
+        video.srcObject = stream
+        video.onloadeddata = drawBackground
+
+        videoRef.current = video
       } catch (error) {
         console.error('Error opening video camera.', error)
       }
     }
 
+    async function drawBackground() {
+      const segmentation = await netRef.current!.segmentPerson(
+        videoRef.current!,
+      )
+      bodyPix.drawBokehEffect(
+        canvasRef.current!,
+        videoRef.current!,
+        segmentation,
+      )
+
+      requestAnimationFrame(drawBackground)
+    }
+
     setupCamera()
   }, [])
 
-  return <video ref={videoRef} autoPlay playsInline controls={false} />
+  useEffect(() => {
+    async function loadBodyPixModel() {
+      netRef.current = await bodyPix.load()
+    }
+
+    loadBodyPixModel()
+  }, [])
+
+  return <canvas ref={canvasRef}></canvas>
 }
 
 export default App
