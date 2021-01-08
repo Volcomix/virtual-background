cd3249b31ed96dca16b0c47cf0a0e3c52d65c9cb
Volcomix <Volcomix@users.noreply.github.com>
2021-01-08 11:49:44 +0100

    Add framerate monitoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import * as bodyPix from '@tensorflow-models/body-pix'
 import React, { useEffect, useRef } from 'react'
+import Stats from 'stats.js'
 import './App.css'
 
 function App() {
-  const videoRef = useRef<HTMLVideoElement>()
-  const netRef = useRef<bodyPix.BodyPix>()
   const canvasRef = useRef<HTMLCanvasElement>(null)
 
   useEffect(() => {
+    const stats = new Stats()
+    stats.showPanel(0)
+    document.body.appendChild(stats.dom)
+
+    const video = document.createElement('video')
+    video.autoplay = true
+    video.playsInline = true
+    video.controls = false
+    video.onloadeddata = drawBackground
+
+    let bodyPixNet: bodyPix.BodyPix
+    let drawBackgroundHandle: number
+
+    async function loadBodyPix() {
+      bodyPixNet = await bodyPix.load()
+    }
+
     async function setupCamera() {
       try {
         const stream = await navigator.mediaDevices.getUserMedia({
@@ -15,47 +31,38 @@ function App() {
         })
         const [videoTrack] = stream.getVideoTracks()
         const { width, height } = videoTrack.getSettings()
-
-        const video = document.createElement('video')
-        video.autoplay = true
-        video.playsInline = true
-        video.controls = false
+        video.srcObject = stream
         video.width = width!
         video.height = height!
-        video.srcObject = stream
-        video.onloadeddata = drawBackground
-
-        videoRef.current = video
       } catch (error) {
         console.error('Error opening video camera.', error)
       }
     }
 
     async function drawBackground() {
-      const segmentation = await netRef.current!.segmentPerson(
-        videoRef.current!
-      )
-      bodyPix.drawBokehEffect(
-        canvasRef.current!,
-        videoRef.current!,
-        segmentation
-      )
+      // Wait for BodyPix model to be loaded
+      if (bodyPixNet) {
+        stats.begin()
 
-      requestAnimationFrame(drawBackground)
+        const segmentation = await bodyPixNet.segmentPerson(video)
+        bodyPix.drawBokehEffect(canvasRef.current!, video, segmentation)
+
+        stats.end()
+      }
+
+      drawBackgroundHandle = requestAnimationFrame(drawBackground)
     }
 
+    loadBodyPix()
     setupCamera()
-  }, [])
 
-  useEffect(() => {
-    async function loadBodyPixModel() {
-      netRef.current = await bodyPix.load()
+    return () => {
+      cancelAnimationFrame(drawBackgroundHandle)
+      stats.dom.remove()
     }
-
-    loadBodyPixModel()
   }, [])
 
-  return <canvas ref={canvasRef}></canvas>
+  return <canvas className="App-canvas" ref={canvasRef}></canvas>
 }
 
 export default App
