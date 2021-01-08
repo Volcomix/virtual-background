5042237dd6d77992d5da8f7c2fee2436a9e97a8b
Volcomix <Volcomix@users.noreply.github.com>
2021-01-08 16:54:25 +0100

    Draw background behind segmented person

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import * as bodyPix from '@tensorflow-models/body-pix'
 import React, { useEffect, useRef } from 'react'
 import Stats from 'stats.js'
 import './App.css'
+import controlRoomImage from "./backgrounds/800px-Main_Control_Room_at_ESA's_Space_Operations_Centre_ESA11252261.jpg"
 
 function App() {
   const canvasRef = useRef<HTMLCanvasElement>(null)
 
   useEffect(() => {
+    const canvas = canvasRef.current!
+    const ctx = canvas.getContext('2d')!
+
     const stats = new Stats()
     stats.showPanel(0)
     document.body.appendChild(stats.dom)
@@ -16,6 +20,15 @@ function App() {
     video.playsInline = true
     video.controls = false
     video.onloadeddata = drawBackground
+
+    const backgroundImage = new Image()
+    backgroundImage.src = controlRoomImage
+
+    let mask: ImageData
+
+    let videoWidth: number
+    let videoHeight: number
+    let videoPixelCount: number
 
     let bodyPixNet: bodyPix.BodyPix
     let drawBackgroundHandle: number
@@ -30,10 +43,22 @@ function App() {
           video: true,
         })
         const [videoTrack] = stream.getVideoTracks()
-        const { width, height } = videoTrack.getSettings()
+        const videoSettings = videoTrack.getSettings()
+        videoWidth = videoSettings.width!
+        videoHeight = videoSettings.height!
+        videoPixelCount = videoWidth * videoHeight
+
         video.srcObject = stream
-        video.width = width!
-        video.height = height!
+        video.width = videoWidth
+        video.height = videoHeight
+
+        backgroundImage.width = videoWidth
+        backgroundImage.height = videoHeight
+
+        canvas.width = videoWidth
+        canvas.height = videoHeight
+
+        mask = new ImageData(videoWidth, videoHeight)
       } catch (error) {
         console.error('Error opening video camera.', error)
       }
@@ -45,7 +70,15 @@ function App() {
         stats.begin()
 
         const segmentation = await bodyPixNet.segmentPerson(video)
-        bodyPix.drawBokehEffect(canvasRef.current!, video, segmentation)
+        for (let i = 0; i < videoPixelCount; i++) {
+          // Set only the alpha component of each pixel
+          mask.data[i * 4 + 3] = segmentation.data[i] ? 255 : 0
+        }
+        ctx.putImageData(mask, 0, 0)
+        ctx.globalCompositeOperation = 'source-in'
+        ctx.drawImage(video, 0, 0)
+        ctx.globalCompositeOperation = 'destination-over'
+        ctx.drawImage(backgroundImage, 0, 0)
 
         stats.end()
       }
