873048536ae291baf2d628cb3ad800245027d6a8
Volcomix <Volcomix@users.noreply.github.com>
2021-01-13 20:28:11 +0100

    Demo WASM function integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
+import { useEffect } from 'react'
 import VideoPlayer from './components/VideoPlayer'
 import useBodyPix from './hooks/useBodyPix'
+
+declare function createTFLiteModule(): Promise<{
+  _add(a: number, b: number): number
+}>
 
 function App() {
   // Load BodyPix only once outside of VideoPlayer component to prevent
   // GPU memory issues with Create React App HMR
   const bodyPixNeuralNetwork = useBodyPix()
+
+  // Demo effect to test WASM function integration
+  useEffect(() => {
+    async function initTFLite() {
+      const Module = await createTFLiteModule()
+      console.log('Demo WASM function result:', Module._add(1, 2))
+    }
+
+    initTFLite()
+  }, [])
 
   return (
     bodyPixNeuralNetwork && (
---
+++ wasm/tflite.cc
@@ -0,0 +1,8 @@
+extern "C" {
+
+  // Demo function to test WASM function integration
+  int add(int a, int b) {
+    return a + b;
+  }
+
+}
