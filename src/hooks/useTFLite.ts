9824974e3c55b6f4b0fa16e4e9eceb3da303e4b2
Volcomix <Volcomix@users.noreply.github.com>
2021-01-19 20:13:35 +0100

    Extract TFLite loading in a hook

---
+++ src/hooks/useTFLite.ts
@@ -0,0 +1,73 @@
+import { useEffect } from 'react'
+
+declare function createTFLiteModule(): Promise<TFLiteModule>
+
+interface TFLiteModule extends EmscriptenModule {
+  _getModelBufferMemoryOffset(): number
+  _getInputMemoryOffset(): number
+  _getInputHeight(): number
+  _getInputWidth(): number
+  _getInputChannelCount(): number
+  _getOutputMemoryOffset(): number
+  _getOutputHeight(): number
+  _getOutputWidth(): number
+  _getOutputChannelCount(): number
+  _loadModel(bufferSize: number): number
+  _runInference(): number
+}
+
+function useTFLite() {
+  useEffect(() => {
+    async function loadTFLite() {
+      // TODO Fetch every resources in parallel
+      // TODO Load full or light model depending on the environment
+      // TODO Detect WASM features to handle SIMD and multithreading
+      const tfliteModule = await createTFLiteModule()
+      const lightModelResponse = await fetch(
+        `${process.env.PUBLIC_URL}/models/segm_lite_v681.tflite`
+      )
+      const lightModel = await lightModelResponse.arrayBuffer()
+      console.log('Lite model buffer size:', lightModel.byteLength)
+
+      const fullModelResponse = await fetch(
+        `${process.env.PUBLIC_URL}/models/segm_full_v679.tflite`
+      )
+      const fullModel = await fullModelResponse.arrayBuffer()
+      console.log('Full model buffer size:', fullModel.byteLength)
+
+      const modelBufferOffset = tfliteModule._getModelBufferMemoryOffset()
+      console.log('Model buffer memory offset:', modelBufferOffset)
+      console.log('Loading light model buffer...')
+      tfliteModule.HEAPU8.set(new Uint8Array(lightModel), modelBufferOffset)
+      console.log(
+        '_loadModel result:',
+        tfliteModule._loadModel(lightModel.byteLength)
+      )
+
+      console.log('Input memory offset:', tfliteModule._getInputMemoryOffset())
+      console.log('Input height:', tfliteModule._getInputHeight())
+      console.log('Input width:', tfliteModule._getInputWidth())
+      console.log('Input channels:', tfliteModule._getInputChannelCount())
+
+      console.log(
+        'Output memory offset:',
+        tfliteModule._getOutputMemoryOffset()
+      )
+      console.log('Output height:', tfliteModule._getOutputHeight())
+      console.log('Output width:', tfliteModule._getOutputWidth())
+      console.log('Output channels:', tfliteModule._getOutputChannelCount())
+
+      // TODO Move inference to the real-time render loop
+      const start = Date.now()
+      const inferenceResult = tfliteModule._runInference()
+      const inferenceDuration = Date.now() - start
+      console.log(
+        `_runInference result: ${inferenceResult} (${inferenceDuration}ms)`
+      )
+    }
+
+    loadTFLite()
+  }, [])
+}
+
+export default useTFLite
