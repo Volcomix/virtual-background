2e94bf6a5cb1f43a6009ababaa49288d29c44ad8
Volcomix <Volcomix@users.noreply.github.com>
2021-01-20 18:38:56 +0100

    Init source image selection

--- src/components/SourceSelectionCard.tsx
+++ src/components/SourceSelectionCard.tsx
@@ -1,12 +1,50 @@
+import Button from '@material-ui/core/Button'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
 
+const imageUrls = [
+  'girl-919048_1280',
+  'doctor-5871743_640',
+  'woman-5883428_1280',
+].map((imageName) => `${process.env.PUBLIC_URL}/images/${imageName}.jpg`)
+
+// TODO Handle cameras and videos
+// const videoUrls = ['1615284309', '1814594990', '1992432523'].map(
+//   (videoName) => `${process.env.PUBLIC_URL}/videos/${videoName}.mp4`
+// )
+
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       height: '100%',
+    },
+    sourceButton: {
+      padding: 0,
+      minWidth: 60,
+      height: 60,
+      width: 60,
+      marginRight: theme.spacing(1),
+      marginBottom: theme.spacing(1),
+      border: '2px solid transparent',
+      alignItems: 'stretch',
+      transition: `transform ${theme.transitions.duration.short}ms ${theme.transitions.easing.easeInOut}`,
+
+      '&:hover': {
+        transform: 'scale(1.12)',
+      },
+    },
+    sourceButtonActive: {
+      borderColor: theme.palette.primary.main,
+      transform: 'scale(1.12)',
+    },
+    sourceImage: {
+      width: 'calc(100% + 2px)',
+      height: 'calc(100% + 2px)',
+      margin: -1,
+      borderRadius: 4,
+      objectFit: 'cover',
     },
   })
 )
@@ -20,6 +58,16 @@ function SourceSelectionCard() {
         <Typography gutterBottom variant="h5" component="h2">
           Source
         </Typography>
+        {imageUrls.map((imageUrl, i) => (
+          <Button
+            key={imageUrl}
+            className={`${classes.sourceButton} ${
+              i === 0 ? classes.sourceButtonActive : ''
+            }`}
+          >
+            <img className={classes.sourceImage} src={imageUrl} alt="" />
+          </Button>
+        ))}
       </CardContent>
     </Card>
   )
