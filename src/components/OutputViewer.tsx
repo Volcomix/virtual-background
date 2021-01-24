ff8d187d3a65f4e112ac49097ec6672d9c22a411
Volcomix <Volcomix@users.noreply.github.com>
2021-01-24 10:09:21 +0100

    Handle virtual backgrounds

--- src/components/OutputViewer.tsx
+++ src/components/OutputViewer.tsx
@@ -31,6 +31,11 @@ function OutputViewer(props: OutputViewerProps) {
 
   useEffect(() => {
     const ctx = canvasRef.current.getContext('2d')!
+
+    // BodyPix needs width and height properties to be set
+    props.sourcePlayback.htmlElement.width = props.sourcePlayback.width
+    props.sourcePlayback.htmlElement.height = props.sourcePlayback.height
+
     const videoPixelCount =
       props.sourcePlayback.width * props.sourcePlayback.height
 
@@ -39,26 +44,56 @@ function OutputViewer(props: OutputViewerProps) {
       props.sourcePlayback.height
     )
     const maskCanvas = document.createElement('canvas')
+    maskCanvas.width = props.sourcePlayback.width
+    maskCanvas.height = props.sourcePlayback.height
     const maskCtx = maskCanvas.getContext('2d')!
+
+    // The useEffect cleanup function is not enough to stop
+    // the rendering loop when the framerate is low
+    let shouldRender = true
 
     let renderRequestId: number
 
     async function render() {
+      if (!shouldRender) {
+        return
+      }
+
       beginFrame()
+
       if (props.background.type !== 'none') {
         const segmentation = await props.bodyPix.segmentPerson(
           props.sourcePlayback.htmlElement
         )
         for (let i = 0; i < videoPixelCount; i++) {
-          // Set only the alpha component of each pixel
+          // Sets only the alpha component of each pixel
           mask.data[i * 4 + 3] = segmentation.data[i] ? 255 : 0
         }
         maskCtx.putImageData(mask, 0, 0)
       }
-      addFrameEvent()
-      ctx.drawImage(props.sourcePlayback.htmlElement, 0, 0)
-      endFrame()
 
+      addFrameEvent()
+
+      ctx.globalCompositeOperation = 'copy'
+
+      if (props.background.type !== 'none') {
+        // TODO Set as post-processing option
+        ctx.filter = 'blur(2px)' // FIXME Does not work on Safari
+
+        ctx.drawImage(maskCanvas, 0, 0)
+        ctx.globalCompositeOperation = 'source-in'
+      }
+
+      ctx.filter = 'none'
+      ctx.drawImage(props.sourcePlayback.htmlElement, 0, 0)
+
+      if (props.background.type === 'blur') {
+        ctx.globalCompositeOperation = 'destination-over'
+        ctx.filter = 'blur(8px)' // FIXME Does not work on Safari
+        ctx.drawImage(props.sourcePlayback.htmlElement, 0, 0)
+      }
+
+      endFrame()
       renderRequestId = requestAnimationFrame(render)
     }
 
@@ -66,6 +101,7 @@ function OutputViewer(props: OutputViewerProps) {
     console.log('Animation started:', props.sourcePlayback, props.background)
 
     return () => {
+      shouldRender = false
       cancelAnimationFrame(renderRequestId)
       console.log('Animation stopped:', props.sourcePlayback, props.background)
     }
@@ -80,22 +116,26 @@ function OutputViewer(props: OutputViewerProps) {
 
   return (
     <React.Fragment>
-      <Typography className={classes.stats} variant="caption">
-        {stats}
-      </Typography>
+      {props.background.type === 'image' && (
+        <img className={classes.render} src={props.background.url} alt="" />
+      )}
       <canvas
         ref={canvasRef}
-        className={classes.canvas}
+        className={classes.render}
         width={props.sourcePlayback.width}
         height={props.sourcePlayback.height}
       />
+      <Typography className={classes.stats} variant="caption">
+        {stats}
+      </Typography>
     </React.Fragment>
   )
 }
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    canvas: {
+    render: {
+      position: 'absolute',
       width: '100%',
       height: '100%',
       objectFit: 'cover',
