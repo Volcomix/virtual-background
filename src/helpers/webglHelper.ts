f6095b8e5de1c794b4e9d3c1bd8e3d5aca2930f9
Volcomix <Volcomix@users.noreply.github.com>
2021-01-28 20:13:06 +0100

    Make glsl-literal work on VSCode OSS

--- src/helpers/webglHelper.ts
+++ src/helpers/webglHelper.ts
@@ -1,6 +1,10 @@
 /**
- * Use it along with glsl-literal VSCode extension
+ * Use it along with boyswan.glsl-literal VSCode extension
  * to get GLSL syntax highlighting.
  * https://marketplace.visualstudio.com/items?itemName=boyswan.glsl-literal
+ *
+ * On VSCode OSS, boyswan.glsl-literal requires slevesque.shader extension
+ * to be installed as well.
+ * https://marketplace.visualstudio.com/items?itemName=slevesque.shader
  */
 export const glsl = String.raw
