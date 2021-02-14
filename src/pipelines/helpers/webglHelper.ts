f057fb09f573578be462dc66e39184266778eef4
Volcomix <Volcomix@users.noreply.github.com>
2021-02-14 18:28:02 +0100

    Move async read back to webglHelpers.ts

--- src/pipelines/helpers/webglHelper.ts
+++ src/pipelines/helpers/webglHelper.ts
@@ -79,3 +79,68 @@ export function createTexture(
   gl.texStorage2D(gl.TEXTURE_2D, 1, internalformat, width, height)
   return texture
 }
+
+export async function readPixelsAsync(
+  gl: WebGL2RenderingContext,
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+  format: number,
+  type: number,
+  dest: ArrayBufferView
+) {
+  const buf = gl.createBuffer()!
+  gl.bindBuffer(gl.PIXEL_PACK_BUFFER, buf)
+  gl.bufferData(gl.PIXEL_PACK_BUFFER, dest.byteLength, gl.STREAM_READ)
+  gl.readPixels(x, y, width, height, format, type, 0)
+  gl.bindBuffer(gl.PIXEL_PACK_BUFFER, null)
+
+  await getBufferSubDataAsync(gl, gl.PIXEL_PACK_BUFFER, buf, 0, dest)
+
+  gl.deleteBuffer(buf)
+  return dest
+}
+
+async function getBufferSubDataAsync(
+  gl: WebGL2RenderingContext,
+  target: number,
+  buffer: WebGLBuffer,
+  srcByteOffset: number,
+  dstBuffer: ArrayBufferView,
+  dstOffset?: number,
+  length?: number
+) {
+  const sync = gl.fenceSync(gl.SYNC_GPU_COMMANDS_COMPLETE, 0)!
+  gl.flush()
+
+  await clientWaitAsync(gl, sync, 0, 10)
+  gl.deleteSync(sync)
+
+  gl.bindBuffer(target, buffer)
+  gl.getBufferSubData(target, srcByteOffset, dstBuffer, dstOffset, length)
+  gl.bindBuffer(target, null)
+}
+
+function clientWaitAsync(
+  gl: WebGL2RenderingContext,
+  sync: WebGLSync,
+  flags: number,
+  intervalMs: number
+) {
+  return new Promise<void>((resolve, reject) => {
+    function test() {
+      const res = gl.clientWaitSync(sync, flags, 0)
+      if (res === gl.WAIT_FAILED) {
+        reject()
+        return
+      }
+      if (res === gl.TIMEOUT_EXPIRED) {
+        setTimeout(test, intervalMs)
+        return
+      }
+      resolve()
+    }
+    test()
+  })
+}
