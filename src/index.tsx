cf4b854892a2365c6a368682a5ff1e45c0b3eeed
Volcomix <Volcomix@users.noreply.github.com>
2022-09-17 10:17:21 +0200

    Upgrade to react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
 import CssBaseline from '@material-ui/core/CssBaseline'
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root')!)
+
+root.render(
   <React.StrictMode>
     <CssBaseline />
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
